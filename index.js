c58fcd10dd8a1d158ece97cae97fdcb0447a1a20
Khallekan <okeolalekanisaac@gmail.com>
2020-12-23 19:06:06 +0100

    fixed the expanding problem

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const problem = document.getElementsByClassName('problem');
 // const answer = document.getElementsByClassName('answer');
+let openPanel,active
+const showAnswer = (e) => {
+    let button = e.target.closest('.problem');
+    const panel = button.nextElementSibling
+
+    if(panel.hasAttribute('hidden')) {
+        panel.removeAttribute('hidden');
+        button.classList.add("active");
+    }else {
+        panel.setAttribute('hidden', true);
+        button.classList.remove("active");
+    }
+    if(openPanel) {
+        openPanel.setAttribute('hidden', true);
+        active.classList.remove("active");
+    }
+    openPanel = panel;
+    active = button;
+}
 
 for (let i = 0; i < problem.length; i++) {
     
     const e = problem[i];
-    e.addEventListener("click", (a)=> {
-        e.classList.toggle('active');
-        let answer = e.nextElementSibling.classList.toggle('show');
-    })
-    
-}
-
+    e.addEventListener("click", showAnswer)
+}    
